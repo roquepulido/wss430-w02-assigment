da3af3dde86df19883873a1bfd340ea6c96444aa
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2025-07-14 00:59:46 -0600

    ADD Chapter 8

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...'); //todo remove this line
+    await new Promise((resolve) => setTimeout(resolve, 3000)); //todo remove this line
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.'); //todo remove this line
 
     return data;
   } catch (error) {
